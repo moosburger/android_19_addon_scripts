9a2e4e1eef9ee3bee31e6a83f691d0e72fc23a00
moosburger <moosburger@freenet.de>
2024-05-06 09:06:09 +0200

    LOS Supported nun Spoofing

--- Scripte/make_Target_200.sh
+++ Scripte/make_Target_200.sh
@@ -40,6 +40,10 @@ echo "Python Version: $version"
 # Import
 ##########################################################################################################
 
+#private static final Signature MICROG_FAKE_SIGNATURE = new Signature("308204433082032ba003020102020900c2e08746644a308d300d06092a864886f70d01010405003074310b3009060355040613025553311330110603550408130a43616c69666f726e6961311630140603550407130d4d6f756e7461696e205669657731143012060355040a130b476f6f676c6520496e632e3110300e060355040b1307416e64726f69643110300e06035504031307416e64726f6964301e170d3038303832313233313333345a170d3336303130373233313333345a3074310b3009060355040613025553311330110603550408130a43616c69666f726e6961311630140603550407130d4d6f756e7461696e205669657731143012060355040a130b476f6f676c6520496e632e3110300e060355040b1307416e64726f69643110300e06035504031307416e64726f696430820120300d06092a864886f70d01010105000382010d00308201080282010100ab562e00d83ba208ae0a966f124e29da11f2ab56d08f58e2cca91303e9b754d372f640a71b1dcb130967624e4656a7776a92193db2e5bfb724a91e77188b0e6a47a43b33d9609b77183145ccdf7b2e586674c9e1565b1f4c6a5955bff251a63dabf9c55c27222252e875e4f8154a645f897168c0b1bfc612eabf785769bb34aa7984dc7e2ea2764cae8307d8c17154d7ee5f64a51a44a602c249054157dc02cd5f5c0e55fbef8519fbe327f0b1511692c5a06f19d18385f5c4dbc2d6b93f68cc2979c70e18ab93866b3bd5db8999552a0e3b4c99df58fb918bedc182ba35e003c1b4b10dd244a8ee24fffd333872ab5221985edab0fc0d0b145b6aa192858e79020103a381d93081d6301d0603551d0e04160414c77d8cc2211756259a7fd382df6be398e4d786a53081a60603551d2304819e30819b8014c77d8cc2211756259a7fd382df6be398e4d786a5a178a4763074310b3009060355040613025553311330110603550408130a43616c69666f726e6961311630140603550407130d4d6f756e7461696e205669657731143012060355040a130b476f6f676c6520496e632e3110300e060355040b1307416e64726f69643110300e06035504031307416e64726f6964820900c2e08746644a308d300c0603551d13040530030101ff300d06092a864886f70d010104050003820101006dd252ceef85302c360aaace939bcff2cca904bb5d7a1661f8ae46b2994204d0ff4a68c7ed1a531ec4595a623ce60763b167297a7ae35712c407f208f0cb109429124d7b106219c084ca3eb3f9ad5fb871ef92269a8be28bf16d44c8d9a08e6cb2f005bb3fe2cb96447e868e731076ad45b33f6009ea19c161e62641aa99271dfd5228c5c587875ddb7f452758d661f6cc0cccb7352e424cc4365c523532f7325137593c4ae341f4db41edda0d0b1071a7c440f0fe9ea01cb627ca674369d084bd2fd911ff06cdbf2cfa10dc0f893ae35762919048c7efc64c7144178342f70581c9de573af55b390dd7fdb9418631895d5f759f30112687ff621410c069308a");
+
+ #private static final Signature MICROG_REAL_SIGNATURE = new Signature("308202ed308201d5a003020102020426ffa009300d06092a864886f70d01010b05003027310b300906035504061302444531183016060355040a130f4e4f47415050532050726f6a656374301e170d3132313030363132303533325a170d3337303933303132303533325a3027310b300906035504061302444531183016060355040a130f4e4f47415050532050726f6a65637430820122300d06092a864886f70d01010105000382010f003082010a02820101009a8d2a5336b0eaaad89ce447828c7753b157459b79e3215dc962ca48f58c2cd7650df67d2dd7bda0880c682791f32b35c504e43e77b43c3e4e541f86e35a8293a54fb46e6b16af54d3a4eda458f1a7c8bc1b7479861ca7043337180e40079d9cdccb7e051ada9b6c88c9ec635541e2ebf0842521c3024c826f6fd6db6fd117c74e859d5af4db04448965ab5469b71ce719939a06ef30580f50febf96c474a7d265bb63f86a822ff7b643de6b76e966a18553c2858416cf3309dd24278374bdd82b4404ef6f7f122cec93859351fc6e5ea947e3ceb9d67374fe970e593e5cd05c905e1d24f5a5484f4aadef766e498adf64f7cf04bddd602ae8137b6eea40722d0203010001a321301f301d0603551d0e04160414110b7aa9ebc840b20399f69a431f4dba6ac42a64300d06092a864886f70d01010b0500038201010007c32ad893349cf86952fb5a49cfdc9b13f5e3c800aece77b2e7e0e9c83e34052f140f357ec7e6f4b432dc1ed542218a14835acd2df2deea7efd3fd5e8f1c34e1fb39ec6a427c6e6f4178b609b369040ac1f8844b789f3694dc640de06e44b247afed11637173f36f5886170fafd74954049858c6096308fc93c1bc4dd5685fa7a1f982a422f2a3b36baa8c9500474cf2af91c39cbec1bc898d10194d368aa5e91f1137ec115087c31962d8f76cd120d28c249cf76f4c70f5baa08c70a7234ce4123be080cee789477401965cfe537b924ef36747e8caca62dfefdd1a6288dcb1c4fd2aaa6131a7ad254e9742022cfd597d2ca5c660ce9e41ff537e5a4041e37");
+
 #++++++++++++++++++++++++++++++++++#
 # Ant+ mitbauen
 AntPlusBuild=false
@@ -64,7 +68,7 @@ repoSync=true
 
 #++++++++++++++++++++++++++++++++++#
 # RepoPick, wenn Gerrit nicht rechtzeitig gepusht wurde
-repoPick=true
+repoPick=false
 # der zu pickende Commit
 gerritSecurityPatch=T_asb_2024-01
 #++++++++++++++++++++++++++++++++++#
@@ -93,27 +97,27 @@ maxArrCnt=0
 patchfolder="packages"
 scriptFolder="android_20_addon_scripts"
 declare -A FilePatch
-spoofing1=0
-FilePatch[$spoofing1,0]="Signature Spoofing"
-FilePatch[$spoofing1,1]="$RootPfad/$patchfolder/$scriptFolder/microG/signature_spoofing_patches/android_frameworks_base-Android13.patch"
-FilePatch[$spoofing1,2]="$RootPfad/$AndroidPath/frameworks/base"
+#spoofing1=0
+#FilePatch[$spoofing1,0]="Signature Spoofing"
+#FilePatch[$spoofing1,1]="$RootPfad/$patchfolder/$scriptFolder/microG/signature_spoofing_patches/android_frameworks_base-Android13.patch"
+#FilePatch[$spoofing1,2]="$RootPfad/$AndroidPath/frameworks/base"
 
-spoofing2=1
-FilePatch[$spoofing2,0]="Signature Spoofing"
-FilePatch[$spoofing2,1]="$RootPfad/$patchfolder/$scriptFolder/microG/signature_spoofing_patches/packages_modules_Permission-Android13.patch"
-FilePatch[$spoofing2,2]="$RootPfad/$AndroidPath/packages/modules/Permission"
+#spoofing2=1
+#FilePatch[$spoofing2,0]="Signature Spoofing"
+#FilePatch[$spoofing2,1]="$RootPfad/$patchfolder/$scriptFolder/microG/signature_spoofing_patches/packages_modules_Permission-Android13.patch"
+#FilePatch[$spoofing2,2]="$RootPfad/$AndroidPath/packages/modules/Permission"
 
-NtpServer=2
+NtpServer=0
 FilePatch[$NtpServer,0]="Ntp Server"
 FilePatch[$NtpServer,1]="$RootPfad/$patchfolder/$scriptFolder/deGoogle/frameworks_base_core_res_res_values_config.patch"
 FilePatch[$NtpServer,2]="$RootPfad/$AndroidPath/frameworks/base"
 
-CapPort=3
+CapPort=1
 FilePatch[$CapPort,0]="Captive Portal check"
 FilePatch[$CapPort,1]="$RootPfad/$patchfolder/$scriptFolder/deGoogle/packages_modules_NetworkStack_res_values_config.patch"
 FilePatch[$CapPort,2]="$RootPfad/$AndroidPath/packages/modules/NetworkStack"
 
-SUPL=4
+SUPL=2
 FilePatch[$SUPL,0]="SUPL No IMSI"
 FilePatch[$SUPL,1]="$RootPfad/$patchfolder/$scriptFolder/deGoogle/SUPL_No_IMSI.patch"
 FilePatch[$SUPL,2]="$RootPfad/$AndroidPath/frameworks/base"
@@ -146,9 +150,10 @@ maxArrCnt=$((${#FilePatch[@]}/3-1))
             patchUndo=("vendor/lineage"
             "frameworks/base"
             "packages/modules/NetworkStack"
-            "packages/modules/Permission"
             "device/google/sunfish"
+            "kernel/google/msm-4.14"
 )
+            #"packages/modules/Permission"
 
 # Generell wohl besser alle Änderungen von mir als patch beim build einzuspielen, damit könnten Kernel updates einfacher werden
 ##########################################################################################################
@@ -306,7 +311,7 @@ function prepCache {
     # Cache Einstellungen
     export USE_CCACHE=1
     export CCACHE_EXEC=/usr/bin/ccache
-    ccache -M 75G
+    ccache -M 100G
     ccache -o compression=true
 }
 
@@ -345,10 +350,10 @@ function applyGitPatches {
         cd "${FilePatch[$i,2]}"
         echo $cve
 
-        #echo - git --stat $cve
+        echo - git apply --stat $cve
         git apply --stat $cve --whitespace=nowarn --ignore-space-change --ignore-whitespace
 
-        #echo - git --check
+        #echo - git apply --check $cve --whitespace=nowarn --ignore-space-change --ignore-whitespace
         # suppress error
         exec 3>&2
         exec 2> /dev/null
@@ -388,6 +393,8 @@ function removeGitPatches {
             echo - not found
         fi
     done
+
+    cd $RootPfad/$AndroidPath
 }
 
 ##########################################################################################################
@@ -433,16 +440,17 @@ echo
 # was bauen
 whatToBuild
 
+restoreBuildEnv
+removeGitPatches
+
 # Nur aufraeumen
 if [ $cleanOnly = true ]
 then
-    restoreBuildEnv
-    removeGitPatches
     echo
-#        if [ -f $RootPfad/$AndroidPath/.repo/projects/external/chromium-webview.git/shallow.lock ]
-  #      then
-    #        rm $RootPfad/$AndroidPath/.repo/projects/external/chromium-webview.git/shallow.lock
-      #  fi
+        if [ -f $RootPfad/$AndroidPath/.repo/projects/external/chromium-webview.git/shallow.lock ]
+        then
+            rm $RootPfad/$AndroidPath/.repo/projects/external/chromium-webview.git/shallow.lock
+        fi
     exit
 fi
 
@@ -465,9 +473,9 @@ echo
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "+++++++++++++++++++++++++++++++++++ Init Repo +++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+pwd
 echo - $RepoCmd init -u https://github.com/LineageOS/android.git -b $AndroidPath
 $RepoCmd init -u https://github.com/LineageOS/android.git -b $AndroidPath
-
 if [ $? -ne 0 ]
 then
     quit "repo Init" "$LINENO"
@@ -479,7 +487,7 @@ then
     echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
     echo "+++++++++++++++++++++++++++++++++++ Sync Repo +++++++++++++++++++++++++++++++++++++++++++++++++++++++"
     echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo -c --force-remove-dirty --force-sync --verbose
+    echo $RepoCmd sync -c --force-remove-dirty --force-sync --verbose
     $RepoCmd sync -c --force-remove-dirty --force-sync --verbose
     echo
 
@@ -574,6 +582,10 @@ do
 
     #zurueck in den Pfad
     cd $lstPath
+
+
+#https://github.com/LineageOS/android_kernel_google_msm-4.14.git -b lineage-20
+#https://github.com/LineageOS/android_device_google_sunfish.git -b lineage-20
 
 ##########################################################################################################
 # Files austauschen, patchen, hinzufuegen
@@ -760,6 +772,6 @@ echo "+++++++++++++++++++++++++++++++++++ aufraeumen +++++++++++++++++++++++++++
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo
 restoreBuildEnv
-removeGitPatches
+#removeGitPatches
 
 ##########################################################################################################
